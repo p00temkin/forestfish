9b2c74c33f660c3c3a0b8e13786675127f7ce67a
p00temkin <p00temkin@gmail.com>
2022-04-30 10:51:12 +0200

    +test cases

---
+++ src/main/java/crypto/forestfish/utils/HttpRequestUtils.java
@@ -0,0 +1,50 @@
+package crypto.forestfish.utils;
+
+import java.io.IOException;
+import java.net.ConnectException;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpRequestUtils {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HttpRequestUtils.class);
+	
+	public static String getBodyUsingUrlRequest(String url) {
+		try {
+			Document doc = Jsoup.connect(url).ignoreContentType(true).get();
+			String res = Jsoup.parse(doc.toString()).body().text();
+			return res;
+		} catch (ConnectException ce) {
+			LOGGER.warn("Connection exception: " + ce.getMessage());
+		} catch (HttpStatusException he) {
+			LOGGER.warn("HTTP status exception: " + he.getMessage());
+		} catch (IOException e) {
+			LOGGER.warn("e: " + e.getMessage());
+			LOGGER.warn("exception class: " + e.getClass());
+		}
+		return "";
+	}
+	
+	public static String getBodyUsingUrlPOSTRequestWithJsonBody(String url, String jsonBody) {
+		try {
+			Document doc = Jsoup.connect(url).ignoreContentType(true)
+					.requestBody(jsonBody)
+					.header("Content-Type", "application/json")
+					.post();
+			String res = Jsoup.parse(doc.toString()).body().text();
+			return res;
+		} catch (ConnectException ce) {
+			LOGGER.warn("Connection exception: " + ce.getMessage());
+		} catch (HttpStatusException he) {
+			LOGGER.warn("HTTP status exception: " + he.getMessage());
+		} catch (IOException e) {
+			LOGGER.warn("e: " + e.getMessage());
+			LOGGER.warn("exception class: " + e.getClass());
+		}
+		return "";
+	}
+}
